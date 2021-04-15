c0500101b7d5bc2611d1f01158126d39f80de49d
mandy845 <82159006+mandy845@users.noreply.github.com>
2021-04-15 12:26:22 +0200

    addeed main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+ab=float(input("please enter the size of the side"))
+bc=float(input('please enter the other side'))
+ac=((ab**2)+(bc**2))**(1/2)
+print("the hypotunse of the triangle is:"+ str(ac))
+print("the hypotunse of the triangle is(binary):"+ str(bin(int(ac))))
+
+#task 2
+my_list=[2,56,12,67,1000,32,89,29,44,39,200,11,21]
+print("the maximum numbers is:"+str(max(my_list)))
+print("the minimum numbers is:"+str(min(my_list)))
